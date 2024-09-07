053bad703dda0ed7b8feb562a9fab53f5a78bc37
wongpiwat <wongpiwat.san@gmail.com>
2024-09-07 01:07:31 -0400

    initial config and paths

---
+++ src/paths.ts
@@ -0,0 +1,13 @@
+export const paths = {
+  home: "/",
+  auth: {
+    signIn: "/auth/sign-in",
+    signUp: "/auth/sign-up",
+    resetPassword: "/auth/reset-password",
+  },
+  dashboard: {
+    overview: "/dashboard",
+    settings: "/dashboard/settings",
+  },
+  errors: { notFound: "/errors/not-found" },
+} as const;
