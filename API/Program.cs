e9148fbbed8f2d8fde183728e184496373531279
BiswadeepMazumder <biswadeepmazumder96@gmail.com>
2024-09-13 04:22:27 -0400

    code clean up

--- API/Program.cs
+++ API/Program.cs
@@ -41,8 +41,3 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
