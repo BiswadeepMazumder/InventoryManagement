a1ddd04d96746713cae7d129d4cbd64f6cd9ad8a
wongpiwat <wongpiwat.san@gmail.com>
2024-09-21 17:21:09 -0400

    change hosting folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  output: "export",
+};
 
 export default nextConfig;
