c7622c6ac6c241b832a778596369161ae351f6ac
wongpiwat <wongpiwat.san@gmail.com>
2024-10-24 17:05:02 -0400

    make card clickable and use new icon

--- src/components/dashboard/overview/CurrentOrder.tsx
+++ src/components/dashboard/overview/CurrentOrder.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Stack from "@mui/material/Stack";
+import { useRouter } from "next/navigation";
+
+import {
+  Avatar,
+  Card,
+  CardActionArea,
+  CardContent,
+  Stack,
+  Typography,
+} from "@mui/material";
 import type { SxProps } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
+
 import { ArrowDown as ArrowDownIcon } from "@phosphor-icons/react/dist/ssr/ArrowDown";
 import { ArrowUp as ArrowUpIcon } from "@phosphor-icons/react/dist/ssr/ArrowUp";
-import { Users as UsersIcon } from "@phosphor-icons/react/dist/ssr/Users";
+import { Notebook as NotebookIcon } from "@phosphor-icons/react/dist/ssr/Notebook";
 
 export interface CurrentOrderProps {
   diff?: number;
-  trend: "up" | "down";
+  trend?: "up" | "down";
   sx?: SxProps;
   value: string;
 }
@@ -22,6 +28,12 @@ export function CurrentOrder({
   sx,
   value,
 }: CurrentOrderProps): React.JSX.Element {
+  const router = useRouter();
+
+  const handleViewOrders = () => {
+    router.push("dashboard/orders");
+  };
+
   const TrendIcon = trend === "up" ? ArrowUpIcon : ArrowDownIcon;
   const trendColor =
     trend === "up"
@@ -30,51 +42,53 @@ export function CurrentOrder({
 
   return (
     <Card sx={sx}>
-      <CardContent>
-        <Stack spacing={2}>
-          <Stack
-            direction="row"
-            sx={{ alignItems: "flex-start", justifyContent: "space-between" }}
-            spacing={3}
-          >
-            <Stack spacing={1}>
-              <Typography color="text.secondary" variant="overline">
-                Current Order
-              </Typography>
-              <Typography variant="h4">{value}</Typography>
-            </Stack>
-            <Avatar
-              sx={{
-                backgroundColor: "var(--mui-palette-success-main)",
-                height: "56px",
-                width: "56px",
-              }}
+      <CardActionArea sx={sx} onClick={handleViewOrders}>
+        <CardContent>
+          <Stack spacing={2}>
+            <Stack
+              direction="row"
+              sx={{ alignItems: "flex-start", justifyContent: "space-between" }}
+              spacing={3}
             >
-              <UsersIcon fontSize="var(--icon-fontSize-lg)" />
-            </Avatar>
-          </Stack>
-          {diff ? (
-            <Stack sx={{ alignItems: "center" }} direction="row" spacing={2}>
-              <Stack
-                sx={{ alignItems: "center" }}
-                direction="row"
-                spacing={0.5}
+              <Stack spacing={1}>
+                <Typography color="text.secondary" variant="overline">
+                  Current Order
+                </Typography>
+                <Typography variant="h4">{value}</Typography>
+              </Stack>
+              <Avatar
+                sx={{
+                  backgroundColor: "var(--mui-palette-success-main)",
+                  height: "56px",
+                  width: "56px",
+                }}
               >
-                <TrendIcon
-                  color={trendColor}
-                  fontSize="var(--icon-fontSize-md)"
-                />
-                <Typography color={trendColor} variant="body2">
-                  {diff}%
+                <NotebookIcon fontSize="var(--icon-fontSize-lg)" />
+              </Avatar>
+            </Stack>
+            {diff ? (
+              <Stack sx={{ alignItems: "center" }} direction="row" spacing={2}>
+                <Stack
+                  sx={{ alignItems: "center" }}
+                  direction="row"
+                  spacing={0.5}
+                >
+                  <TrendIcon
+                    color={trendColor}
+                    fontSize="var(--icon-fontSize-md)"
+                  />
+                  <Typography color={trendColor} variant="body2">
+                    {diff}%
+                  </Typography>
+                </Stack>
+                <Typography color="text.secondary" variant="caption">
+                  Since last month
                 </Typography>
               </Stack>
-              <Typography color="text.secondary" variant="caption">
-                Since last month
-              </Typography>
-            </Stack>
-          ) : null}
-        </Stack>
-      </CardContent>
+            ) : null}
+          </Stack>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
