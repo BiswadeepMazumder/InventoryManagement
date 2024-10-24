32261eec31ba686099f8712dbd09d130162de9d4
wongpiwat <wongpiwat.san@gmail.com>
2024-10-24 17:15:11 -0400

    fix order status schema and type

--- src/types/order.ts
+++ src/types/order.ts
@@ -4,7 +4,7 @@ export interface Order {
   orderName: string;
   userId: string;
   orderAmount: number;
-  orderStatus: 0 | 1 | 2 | 3 | 4 | 5;
+  orderStatus: 0 | 1 | 2 | 3 | 4 | 5 | number;
   cancelComment: string;
   orderItems: OrderItems[];
 }
